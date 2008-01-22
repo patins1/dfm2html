acd735ef61c987fd935d1024ae7c984028c0c3de
Joerg Kiegeland <joerg@kiegeland.com>
2008-01-22 22:50:32 +0000

    Do not generated <NOSCRIPT> element if already provided by user or if it is a page in a <iframe>
    git-svn-id: svn://localhost/dfm2repos@50 52dd2445-3c6a-a148-8b57-78e9b6f07541

--- HBuilder/uConversion.pas
+++ HBuilder/uConversion.pas
@@ -2671,7 +2671,7 @@ begin
   preloadOneByOne:='';
   if Need1by1 then
    preloadOneByOne:=CRLF+MakeIndent(2)+'<script type="text/javascript">preload(''onebyone.gif'');</script>';
-  if not pagenest.IsIFrame and (AdvPos('<noscript',ns)=0) then
+  if not ((pagenest.parentTS<>nil) and pagenest.parentTS.IsIFrame) and (AdvPos('<noscript',ns)=0) then
    Insert(CRLF+MakeIndent(2)+'<noscript>This page requires JavaScript. <a href="http://www.dfm2html.com/js.html">Help</a></noscript>',ns,r);
   Insert(CRLF+MakeIndent(2)+'<script type="text/javascript" src="dfm2html.js"></script>'+preloadOneByOne,ns,r);
  end;

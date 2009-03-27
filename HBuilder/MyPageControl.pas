75b536279a79cface69d21a6ecb28d6d93ac6155
Joerg Kiegeland <joerg@kiegeland.com>
2009-03-27 10:25:18 +0000

    override DrawTab, for Kylix?
    git-svn-id: svn://localhost/dfm2repos@148 52dd2445-3c6a-a148-8b57-78e9b6f07541

--- HBuilder/MyPageControl.pas
+++ HBuilder/MyPageControl.pas
@@ -6,7 +6,7 @@ uses
   {$IFDEF CLX}
   QControls, QGraphics, QStdCtrls, Qt, QComCtrls, QTntStdCtrls,
   {$ELSE}
-  Controls, Windows, Messages, StdCtrls, ComCtrls, TntComCtrls,
+  Controls, Windows, Messages, StdCtrls, ComCtrls, TntComCtrls, Graphics,
   {$ENDIF}
   SysUtils, Classes,types;
 
@@ -21,6 +21,7 @@ type
       const MousePos: TPoint): Boolean; override;
 {$ENDIF}
 
+    procedure DrawTab(TabIndex: Integer; const Rect: TRect; Active: Boolean); override;
   public
     { Public declarations }
   published
@@ -58,5 +59,49 @@ begin
  result:=true;
 end;
 {$ENDIF}
+
+
+
+procedure TMyPageControl.DrawTab(TabIndex: Integer; const Rect: TRect;
+  Active: Boolean);
+var
+  imageindex: Integer;
+  r: TRect;
+  S: String;
+begin
+  If Assigned( OnDrawTab ) Then
+    inherited
+  Else Begin
+    R:= Rect;
+//    canvas.Brush.Color:=clWhite;
+    Canvas.Fillrect( r );
+    imageindex := GetImageIndex( tabindex );
+    If (imageindex >=0) and Assigned( Images ) Then Begin
+      //SaveDC( canvas.handle );
+      R:=Rect;
+      inc(R.Top,3);
+      if Active then
+       inc(R.Left,4);
+      images.Draw( Canvas, R.Left, R.Top, imageindex, {Pages[TabIndex].enabled}true );
+      // images.draw fouls the canvas colors if it draws
+      // the image disabled, thus the SaveDC/RestoreDC
+      //RestoreDC( canvas.handle, -1 );
+      R:= Rect;
+      R.Left := R.Left + images.Width + 4;
+    End;
+    S:= Tabs[ TabIndex ];
+    InflateRect( r, -4, -3 );
+    inc(r.Bottom,4);
+    if Active then
+     inc(r.Left,4);
+    DrawText( Canvas.handle,
+              PChar(S),
+              Length(S),
+              r,
+              DT_SINGLELINE or DT_LEFT or DT_TOP );
+  End;
+end;
+
+
 
 end.

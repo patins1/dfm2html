51c3feddb7b37e0ab900f55389d0000257acf4cd
Joerg Kiegeland <joerg@kiegeland.com>
2010-01-22 21:51:25 +0000

    refactoring: checked in unversioned unit
    git-svn-id: svn://localhost/dfm2repos@277 52dd2445-3c6a-a148-8b57-78e9b6f07541

---
+++ HBuilder/UnicodeCtrls.pas
@@ -0,0 +1,55 @@
+unit UnicodeCtrls;
+
+interface
+
+uses
+  {$IFDEF CLX}
+  QControls, QGraphics, QStdCtrls, QMask, Qt, QComCtrls, QTntStdCtrls,
+  {$ELSE}
+  Controls, Windows, Messages, Graphics, StdCtrls, ShellAPI, Mask, ComCtrls,Buttons,Forms,Menus,ExtCtrls, {$IFNDEF VER210}TntComCtrls,TntForms,{$ENDIF}
+  {$ENDIF}
+  SysUtils, Classes;
+
+{$IFDEF VER210}
+type
+  TTntToolButton = class(TToolButton) end;
+  TTntSpeedButton = class(TSpeedButton) end;
+  TTntButton = class(TButton) end;
+  TTntForm = class(TForm) end;
+  TTntComboBox = class(TComboBox) end;
+  TTntEdit = class(TEdit) end;
+  TTntMemo = class(TMemo) end;
+  TTntPageControl = class(TPageControl) end;
+  TTntCheckBox = class(TCheckBox) end;
+  TTntGroupBox = class(TGroupBox) end;
+  TTntToolBar = class(TToolBar) end;
+  TTntMainMenu = class(TMainMenu) end;
+  TTntPopupMenu = class(TPopupMenu) end;
+  TTntMenuItem = class(TMenuItem) end;
+  TTntTabSheet = class(TTabSheet) end;
+  TTntRadioButton = class(TRadioButton) end;
+  TTntRadioGroup = class(TRadioGroup) end;
+  TTntListBox = class(TListBox) end;
+{$ENDIF}
+
+procedure Register;
+
+implementation
+
+procedure Register;
+begin
+{$IFDEF VER210}
+  RegisterComponents('TNT', [TTntToolBar]);
+  RegisterComponents('TNT', [TTntMainMenu]);
+  RegisterComponents('TNT', [TTntPopupMenu]);
+  RegisterComponents('TNT', [TTntMenuItem]);
+  RegisterComponents('TNT', [TTntTabSheet]);
+
+  RegisterComponents('TNT', [TTntButton]);
+  RegisterComponents('TNT', [TTntRadioButton]);
+  RegisterComponents('TNT', [TTntRadioGroup]);
+  RegisterComponents('TNT', [TTntListBox]);
+{$ENDIF}
+end;
+
+end.

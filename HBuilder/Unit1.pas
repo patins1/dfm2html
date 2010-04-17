f3790ff3485bd80a1f995f82339aa691ab7cc6a6
Joerg Kiegeland <joerg@kiegeland.com>
2010-04-17 07:37:16 +0000

    fixed bug 2 of bug report 2010-04-14, 12:53:44, 62ms
    git-svn-id: svn://localhost/dfm2repos@323 52dd2445-3c6a-a148-8b57-78e9b6f07541

--- HBuilder/Unit1.pas
+++ HBuilder/Unit1.pas
@@ -2436,6 +2436,7 @@ begin
  begin
  if not Tabs.CommitChanges then exit;
  LateCreateForm(TPageWizard,PageWizard);
+ PageWizard.PageControl1.ActivePage:=PageWizard.TabSheet1;
  PageWizard.ActiveControl:=PageWizard.eHTTPURL;
  //showmessage('You have to specify a HTTP URL');
  ls:=TList.Create;

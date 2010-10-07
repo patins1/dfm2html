996ac3bd6f63199d67acf48e3738e0857568d9f6
Joerg Kiegeland <joerg@kiegeland.com>
2010-10-07 06:59:38 +0000

    make Copy shortcut work in FTP Log memo
    git-svn-id: svn://localhost/dfm2repos@366 52dd2445-3c6a-a148-8b57-78e9b6f07541

--- HBuilder/Unit1.pas
+++ HBuilder/Unit1.pas
@@ -2272,7 +2272,7 @@ var c:TControl;
 begin
  if (Message.CharCode=VK_DELETE) or (Message.CharCode=ord('C')) or (Message.CharCode=ord('X')) or (Message.CharCode=ord('V')) then
  begin
- if ActiveEditControl<>nil then
+ if (ActiveEditControl<>nil) or (Screen.ActiveForm is TPublishLog) then
  begin
   Result:=False;
   exit;
